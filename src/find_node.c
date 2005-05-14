6b3ee52048c142aa2ecbb14d3613289f1a0c368b
stesie <stesie>
2005-05-14 18:20:52 +0000

    adding myself to copyright notices, forgotten before

--- src/find_node.c
+++ src/find_node.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2005  Juergen Stuber <juergen@jstuber.net>, Germany
+ *               2005  Stefan Siegl <ssiegl@gmx.de>, Germany
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

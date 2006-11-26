67c23e8d1f0bfd05288be4c0ef563b20675d91af
stesie <stesie>
2006-11-26 16:07:08 +0000

    initialize whole geier_context to zero on context_new

--- src/context.c
+++ src/context.c
@@ -31,15 +31,14 @@ geier_context *geier_context_new(void)
 	geier_context *context = malloc(sizeof(struct _geier_context));
 	if(! context) return NULL; /* out of memory */
 
+	memset(context, 0, sizeof(geier_context));
+
         context->cert_filename = DEFAULT_CERT_FILE;
         context->xmlsec_tpl_filename = DEFAULT_XMLSEC_TEMPLATE;
 
-	context->clearing_timeout_ms = DEFAULT_CLEARING_TIMEOUT;
+	/* context->clearing_timeout_ms = DEFAULT_CLEARING_TIMEOUT; */
 	context->schema_dir_url = DEFAULT_SCHEMA_DIR_URL;
 	context->stylesheet_dir_url = DEFAULT_STYLESHEET_DIR_URL;
-
-	context->session_key = NULL;
-	context->session_key_len = 0;
 
 	return context;
 };

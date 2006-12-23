ad6a889640adb9250f398419f694cc0f59d1aa66
stesie <stesie>
2006-12-23 14:40:18 +0000

    ported to xulrunner libnss

--- src/dsig-open.c
+++ src/dsig-open.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005  Stefan Siegl <stesie@brokenpipe.de>, Germany
+ * Copyright (C) 2005, 2006  Stefan Siegl <stesie@brokenpipe.de>, Germany
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,29 +21,129 @@
 #endif
 
 #include <geier.h>
-#include <geier-dsig.h>
 #include "context.h"
+#include "dsig.h"
 
-#include <openssl/err.h>
+#include <nss/pk11pub.h>
+#include <nss/p12plcy.h>
 
-PKCS12 *
+/*
+ * Portions of this file are taken from mozilla libnss, which originally
+ * is MPL/GPL dual licensed.  However libgeier is GPL only!
+ *
+ * The Original Code is the Netscape security libraries.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1994-2000 Netscape Communications Corporation.  All
+ * Rights Reserved.
+ *
+ * Portions created by Sun Microsystems, Inc. are Copyright (C) 2003
+ * Sun Microsystems, Inc. All Rights Reserved.
+ *
+ * Contributor(s):
+ *      Dr Vipul Gupta <vipul.gupta@sun.com>, Sun Microsystems Laboratories
+ */
+
+SECStatus
+SECU_FileToItem(SECItem *dst, PRFileDesc *src)
+{
+    PRFileInfo info;
+    PRInt32 numBytes;
+    PRStatus prStatus;
+
+    if (src == PR_STDIN)
+	    return SECFailure;
+
+    prStatus = PR_GetOpenFileInfo(src, &info);
+
+    if (prStatus != PR_SUCCESS)
+	    return SECFailure;
+
+    /* XXX workaround for 3.1, not all utils zero dst before sending */
+    dst->data = 0;
+    if (!SECITEM_AllocItem(NULL, dst, info.size))
+	goto loser;
+
+    numBytes = PR_Read(src, dst->data, info.size);
+    if (numBytes != info.size) 
+	goto loser;
+
+    return SECSuccess;
+
+loser:
+    SECITEM_FreeItem(dst, PR_FALSE);
+    return SECFailure;
+}
+
+
+
+SEC_PKCS12DecoderContext *
 geier_dsig_open(const char *filename, const char *pincode)
 {
-	FILE *handle = fopen(filename, "r");
-	if(! handle) return NULL;
+	SEC_PKCS12DecoderContext *p12dcx = NULL;
+	SECItem p12file = { 0 };
+	SECStatus rv = SECFailure;
 
-	PKCS12 *p12 = NULL;
-	if (!(p12 = d2i_PKCS12_fp(handle, NULL))) {
-		ERR_print_errors_fp(stderr);
-		goto out;
+	/* convert password */
+	int pincode_len = strlen(pincode);
+	SECItem *pwItem = SECITEM_AllocItem(NULL, NULL, (pincode_len + 1) << 1);
+	int i;
+	for(i = 0; i <= pincode_len; i ++) {
+		pwItem->data[(i << 1) + 0] = 0;
+		pwItem->data[(i << 1) + 1] = pincode[i];
 	}
 
-	if(! PKCS12_verify_mac(p12, pincode, -1)) 
-		p12 = NULL; /* MAC is not valid */
+	/* init the decoder context */
+	p12dcx = SEC_PKCS12DecoderStart(pwItem, NULL, NULL, NULL, NULL,
+					NULL, NULL, NULL);
 
- out:
-	fclose(handle);
-	return p12;
+	if(!p12dcx) {
+		fprintf(stderr, PACKAGE_NAME ": unable to start PKCS12 "
+			"decoder.\n");
+		goto loser;
+	}
+
+	PRFileDesc *handle = PR_Open(filename, PR_RDONLY, 0400);
+	if(! handle)
+		goto loser;
+
+	/* decode the item */
+	rv = SECU_FileToItem(&p12file, handle);
+	if (rv != SECSuccess) {
+		fprintf(stderr, PACKAGE_NAME ": unable to read from file.\n");
+		goto loser;
+	}
+
+	rv = SEC_PKCS12DecoderUpdate(p12dcx, p12file.data, p12file.len);
+	if(rv != SECSuccess) {
+		/* error = PR_GetError();
+		 * if(error == SEC_ERROR_DECRYPTION_DISALLOWED) {
+		 * 	PR_SetError(error, 0);
+		 *	goto loser;
+		 * }
+		 */
+
+		fprintf(stderr, PACKAGE_NAME ": PKCS12 decoding failed.\n");
+	}
+
+	/* does the blob authenticate properly? */
+	rv = SEC_PKCS12DecoderVerify(p12dcx);
+	if(rv != SECSuccess) {
+		fprintf(stderr, PACKAGE_NAME ": PKCS12 blob doesn't "
+			"authenticate properly.\n");
+		goto loser;
+	}
+
+loser:
+	if(p12dcx && rv != SECSuccess) {
+		SEC_PKCS12DecoderFinish(p12dcx);
+		p12dcx = NULL;
+	}
+
+	SECITEM_ZfreeItem(pwItem, PR_TRUE);
+
+	return p12dcx;
 }
 
 
@@ -53,9 +153,12 @@ geier_dsig_verify_mac(geier_context *context,
 		      const char *filename, 
 		      const char *pincode)
 {
-	PKCS12 *p12 = geier_dsig_open(filename, pincode);
+        (void) context;
+
+	SEC_PKCS12DecoderContext *p12 = geier_dsig_open(filename, pincode);
 	if(! p12) return 1;
 
-	PKCS12_free(p12);
+	SEC_PKCS12DecoderFinish(p12);
 	return 0;
 }
+

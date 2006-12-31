39fb655353c478e12a2acb789e20885e82e6dc1b
stesie <stesie>
2006-12-31 19:53:22 +0000

    just committing to somewhere save the source which didn't make it in the end, will delete within shortly

--- src/dsig-get-key.c
+++ src/dsig-get-key.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005  Stefan Siegl <stesie@brokenpipe.de>, Germany
+ * Copyright (C) 2005,2006  Stefan Siegl <stesie@brokenpipe.de>, Germany
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,7 +21,6 @@
 #endif
 
 #include <geier.h>
-#include <geier-dsig.h>
 #include "context.h"
 
 #include <string.h>
@@ -29,107 +28,97 @@
 
 #include "dsig.h"
 
+#include <nss/pk11pub.h>
+#include <xmlsec/nss/pkikeys.h>
 
-static EVP_PKEY *
+static xmlSecKeyData *
 geier_dsig_get_key(geier_context *context, 
 		   const char *filename, 
 		   const char *pin,
-		   const char *keyname)
+		   const char *bagname)
 {
-	EVP_PKEY *result = NULL;
+	xmlSecKeyData *result = NULL;
+	SEC_PKCS12DecoderContext *p12 = NULL;
 
-	int pinlen = pin ? strlen(pin) : 0;
-
-	PKCS12 *p12 = geier_dsig_open(filename, pin);
-	if(! p12) return NULL;
-	
-	STACK_OF(PKCS7) *safes = PKCS12_unpack_authsafes(p12);
-	if(! safes) return NULL;
-
-	int i;
-	for(i = 0; i < sk_PKCS7_num(safes); i ++) {
-		PKCS7 *p7 = sk_PKCS7_value(safes, i);
-		assert(p7);
-
-		STACK_OF(PKCS12_SAFEBAGS) *bags = NULL;
-
-		int id = OBJ_obj2nid(p7->type);
-		if(id == NID_pkcs7_data)
-			bags = PKCS12_unpack_p7data(p7);
-
-		else if (id == NID_pkcs7_encrypted)
-			bags = PKCS12_unpack_p7encdata(p7, pin, pinlen);
-
-		else {
-			fprintf(stderr, PACKAGE_NAME "unknown PKCS12_SAFEBAGS "
-				"nid discovered.");
-			goto out0;
-		}
-
-		assert(bags);
-
-		int j;
-		for(j = 0; j < sk_PKCS12_SAFEBAG_num(bags); j ++) {
-			PKCS12_SAFEBAG *bag = sk_PKCS12_SAFEBAG_value(bags, j);
-			assert(bag);
-			
-			if(M_PKCS12_bag_type(bag) == NID_pkcs8ShroudedKeyBag) {
-				char *friendlyname = 
-					geier_dsig_get_attr(bag->attrib, 
-							    "friendlyName");
-
-				if(strcmp(friendlyname, keyname) == 0) {
-					/* got the key we look for */
-					PKCS8_PRIV_KEY_INFO *p8;
-					p8 = PKCS12_decrypt_skey(bag, pin, 
-								 pinlen);
-
-					if(p8)
-						result = EVP_PKCS82PKEY(p8);
-
-					sk_PKCS12_SAFEBAG_pop_free
-						(bags, PKCS12_SAFEBAG_free);
-
-					free(friendlyname);
-					goto out1;
-				}
-
-				free(friendlyname);
-			}
-		}
-
-
-		sk_PKCS12_SAFEBAG_pop_free(bags, PKCS12_SAFEBAG_free);
+	if(geier_dsig_resolve_nickname(context, filename, pin,
+				       &p12, &bagname)) {
+		fprintf(stderr, PACKAGE_NAME ": unable to resolve " 
+			"PKCS#12 bag nickname.\n");
+		goto out;
 	}
 
-out1:
-	
+	fprintf(stderr, "need cert: %s\n", bagname);
 
-out0:
-	sk_PKCS7_pop_free(safes, PKCS7_free);
-	PKCS12_free(p12);
-	
+	CERTCertList *clist = SEC_PKCS12DecoderGetCerts(p12);
+	if(! clist) {
+		fprintf(stderr, PACKAGE_NAME ": unable to get "
+			"certificates list.\n");
+		goto out;
+	}
+
+	CERTCertListNode *node;
+	for(node = CERT_LIST_HEAD(clist);
+	    ! CERT_LIST_END(node, clist);
+	    node = CERT_LIST_NEXT(node)) {
+		CERTCertificate *cert = node->cert;
+
+		if(strcmp(cert->nickname, bagname))
+			continue;
+
+		SECKEYPrivateKey *privkey = PK11_FindKeyByAnyCert(cert, NULL); 
+		if(! privkey) {
+			fprintf(stderr, PACKAGE_NAME ": unable to find "
+				"private key.\n");
+			goto out;
+		}
+
+		SECKEYPublicKey *pubkey = CERT_ExtractPublicKey(cert);
+		if(! pubkey) {
+			fprintf(stderr, PACKAGE_NAME ": unable to extract "
+				"public key from available certificate.\n");
+			goto out;
+		}
+
+		result = xmlSecNssPKIAdoptKey(privkey, pubkey);
+		if(! result) {
+			fprintf(stderr, PACKAGE_NAME ": unable to adopt "
+				"needed keys.\n");
+			goto out;
+		}
+		
+		break;
+	}
+
+	if(! result)
+		fprintf(stderr, PACKAGE_NAME ": unable to match "
+			"certificate.  this should not happen.\n");
+
+ out:
+	SEC_PKCS12DecoderFinish(p12);
 	return result;
 }
 
 
 
-EVP_PKEY *
+xmlSecKeyData *
 geier_dsig_get_signaturekey(geier_context *context, 
 			    const char *filename, 
 			    const char *password)
 {
-	return geier_dsig_get_key(context, filename, password, "signaturekey");
+	return geier_dsig_get_key(context, filename, password,
+				  "signaturekey");
 }
 
 
 
 
-EVP_PKEY *
+xmlSecKeyData *
 geier_dsig_get_encryptionkey(geier_context *context, 
 			    const char *filename, 
 			    const char *password)
 {
-	return geier_dsig_get_key(context, filename, password, "encryptionkey");
+	return geier_dsig_get_key(context, filename, password,
+				  "encryptionkey");
 }
+
 
